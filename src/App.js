950007a7c8807cdc12542691c339e267f40984b0
milanvaghani <102277634+milanvaghani@users.noreply.github.com>
2022-04-18 19:12:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Restaurant from "./component/Basics/Restaurant";
+import UseEffect from "./component/Hooks/UseEffect";
+import UseReducer from "./component/Hooks/UseReducer";
+import UseState from "./component/Hooks/UseState";
+import ToDo from "./component/ToDoList/ToDo";
+import Temp from "./component/Weather/Temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+      {/* <ToDo /> */}
+      {/* <UseReducer /> */}
+      {/* <UseEffect /> */}
+      {/* <UseState /> */}
+      {/* <Restaurant /> */}
+    </>
+  );
+};
+
+export default App;
